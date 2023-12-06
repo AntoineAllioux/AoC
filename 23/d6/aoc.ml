5ee1cdda355827adb16c4bafe36ec336debe9670
Antoine Allioux <antoine.allioux@gmail.com>
2023-12-06 10:08:25 +0100

    more correct

--- 23/d6/aoc.ml
+++ 23/d6/aoc.ml
@@ -5,10 +5,12 @@ open Stdio
 let score (t, d) =
   let tf = Int.to_float t in
   let df = Int.to_float d in
-  let delta = Float.sqrt (tf *. tf -. 4. *. df) in
-  let x1 = (-. tf +. delta) /. -. 2. in
-  let x2 = (-. tf -. delta) /. -. 2. in
-  max 0 (Float.to_int (Float.round_down x2 -. Float.round_up x1) + 1)
+  let delta = tf *. tf -. 4. *. df in
+  if Float.(>=) delta 0. then
+    let x1 = (-. tf +. Float.sqrt delta) /. -. 2. in
+    let x2 = (-. tf -. Float.sqrt delta) /. -. 2. in
+    max 0 (Float.to_int (Float.round_down x2 -. Float.round_up x1) + 1)
+  else 0
 
 let part1 =
   [(48, 261); (93, 1192); (84, 1019); (66, 1063)]
