e4bfec69aaa8a6edba7a805017e53e55b51ebb5a
Antoine Allioux <antoine.allioux@gmail.com>
2023-12-12 21:34:20 +0100

    lil cleanup

--- 23/d12/aoc.ml
+++ 23/d12/aoc.ml
@@ -2,7 +2,6 @@ open Base
 open Stdio
 open List
 open Fn
-open Cartesian_product
 
 type spring =
   | Operational
@@ -10,21 +9,24 @@ type spring =
   | Unknown
 [@@deriving sexp_of, compare, equal]
 
-let sum = reduce_exn ~f:( + ) 
+let sum = reduce_exn ~f:( + )
 
-module Mem = struct
-  module T = struct
-    type t = spring list * int list * bool
-    [@@deriving sexp_of, compare, equal]
-  end
-  include T
-  include Comparator.Make(T)
-end
+let curry f (x, y) = f x y
 
 let arrangements springs damaged_springs =
+  
+  let module Mem = struct
+      module T = struct
+        type t = spring list * int list * bool
+        [@@deriving sexp_of, compare, equal]
+      end
+      include T
+      include Comparator.Make(T)
+    end in
+  
   let mem = ref (Map.empty (module Mem)) in
-  let rec loop springs damaged_springs eating =
-    match Map.find (! mem) (springs, damaged_springs, eating) with
+  let rec loop springs damaged_springs active =
+    match Map.find (! mem) (springs, damaged_springs, active) with
     | Some v -> v
     | _ ->
        let res = 
@@ -36,36 +38,33 @@ let arrangements springs damaged_springs =
          | (Operational :: s, n :: ns) when n = 0 ->
             loop s ns false
          | (Operational :: s, n :: ns) when n > 0 ->
-            if eating
+            if active
             then 0
             else loop s (n :: ns) false
          | (Damaged :: _, []) -> 0
          | (Damaged :: s, n :: ns) when n > 0 ->
             loop s (n - 1 :: ns) true
          | (Damaged :: s, n :: ns) when n = 0 ->
-            if eating
+            if active
             then 0
             else loop (Damaged :: s) ns false
          | (Unknown :: s, []) -> loop s [] false
          | (Unknown :: s, n :: ns) when n > 0 ->
-            if eating
+            if active
             then loop s (n - 1 :: ns) true
             else loop s (n - 1 :: ns) true 
                  + loop s (n :: ns) false
          | (Unknown :: s, n :: ns) when n = 0 ->
-            if eating
+            if active
             then loop s ns false
             else loop (Unknown :: s) ns false in
-       begin
-         mem := Map.add_exn (! mem)
-                  ~key:(springs, damaged_springs, eating)
-                  ~data:res;
-         res
-       end
+       
+       mem := Map.add_exn (! mem)
+                ~key:(springs, damaged_springs, active)
+                ~data:res;
+       res
 
   in loop springs damaged_springs false
-
-let curry f (x, y) = f x y
 
 let _ =
   let parse_entry n s =
@@ -108,7 +107,5 @@ let _ =
     |> map ~f:(curry arrangements)
     |> sum in
   
-  begin
-    printf "Part 1: %d\n" part1;
-    printf "Part 2: %d\n" part2
-  end
+  printf "Part 1: %d\n" part1;
+  printf "Part 2: %d\n" part2
