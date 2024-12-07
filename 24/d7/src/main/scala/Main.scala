54ff7c4f9aebbde0c726df6b063820bcad2b7936
Antoine Allioux <antoine.allioux@gmail.com>
2024-12-08 00:43:06 +0100

    cleanup

--- 24/d7/src/main/scala/Main.scala
+++ 24/d7/src/main/scala/Main.scala
@@ -1,9 +1,32 @@
 package aoc
 
-@main def hello(): Unit =
-  val part1 = aoc.part1.calibration();
-  val part2 = aoc.part2.calibration();
-  println(s"Part1: $part1")
-  println(s"Part1: $part2")
+import scala.io.Source
 
-  
+def isSatisfiable(part2: Boolean)(target: Long, xs: List[Long]): Boolean =
+  xs match
+    case Nil => target == 0 || target == 1
+    case x :: xs =>
+      val digits_nb = Math.log10(x + 1).ceil
+      val n = Math.pow(10, digits_nb).toLong
+
+      (target >= x && isSatisfiable(part2)(target - x, xs))
+      || (target % x == 0 && isSatisfiable(part2)(target / x, xs))
+      || (part2 && target % n == x && isSatisfiable(part2)(target / n, xs))
+
+def calibration(part2: Boolean)(l: List[(Long, List[Long])]): Long =
+  l.filter(isSatisfiable(part2)).map(_._1).sum
+
+@main def main(): Unit =
+  val lines = Source.fromFile("data/input").getLines()
+  val l = lines
+    .map(s =>
+      val l = s.split(Array(' ', ':')).filterNot(_.isEmpty()).map(_.toLong)
+      (l.head, l.tail.toList.reverse)
+    )
+    .toList
+
+  val part1 = calibration(false)(l)
+  val part2 = calibration(true)(l)
+
+  println(s"Part1: $part1")
+  println(s"Part2: $part2")
